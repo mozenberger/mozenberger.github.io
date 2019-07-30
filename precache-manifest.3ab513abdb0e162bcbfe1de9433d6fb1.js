5e5cd4244cd0e5a173d595f22ee4876bb7b57ef2
mozenberger <50963353+mozenberger@users.noreply.github.com>
2019-07-30 12:04:12 -0400

    Adding react as index

---
+++ precache-manifest.3ab513abdb0e162bcbfe1de9433d6fb1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e5bdb3036857e8b7de89edddd592f6",
+    "url": "/ReactJS/index.html"
+  },
+  {
+    "revision": "57e1911fd6ae28bb684c",
+    "url": "/ReactJS/static/js/2.07fea5f9.chunk.js"
+  },
+  {
+    "revision": "5a902ebbc5ccb36ec601",
+    "url": "/ReactJS/static/js/main.b25921ce.chunk.js"
+  },
+  {
+    "revision": "ccd1ca3e8aa5e70a4e03",
+    "url": "/ReactJS/static/js/runtime~main.f8a3c22c.js"
+  }
+]);
